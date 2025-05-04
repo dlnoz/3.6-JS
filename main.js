42e21945e195b1d5355fa4aaf7095c4290e28f0e
Dilnozass <rakhma1ullaevaa@gmail.com>
2025-05-04 15:48:19 +0500

    start

---
+++ main.js
@@ -0,0 +1,82 @@
+//  ------ #TASK1 ------
+
+// let num1 = prompt("Birinchi sonni kiriting...")-0
+// let num2 = prompt("Ikkinchi sonni kiriting...")-0
+// let sum = num1 + num2
+
+// if (sum >= 0 && sum <= 9){
+//     console.log(String(sum))
+// }
+// else if (sum >= 10 && sum <= 99){
+//     Number(sum)
+//     console.log(Number(sum))
+// }
+// else{
+//     sum = null
+//     console.log(null)
+// }
+
+
+
+//  ------ #TASK2 ------
+
+// 1soat = 60minut
+// 1minut = 60sekund
+
+// let secund = prompt("Sekund kiriting...") // 7200 sekund = 120minut = 2soat
+// let hour = secund % min 
+// let min = 
+
+
+
+// ------ #TASK1 ------
+
+// let person = {
+//     name: "Dilnoza",
+//     age: 17,
+//     job: "Study",
+//     interest: "Learning",
+//     hobby: "Watching shorts",
+//     number: "+998 99 465 25 02",
+//     username: "dlnnsz"
+// }
+
+// for(let [key, value] of Object.entries(person)){
+//     console.log(`${key}:${value}`);
+// }
+
+
+
+
+// let arr = [15, 25, 46, 58, 91, 76, 27]
+
+// for(i)
+
+
+
+
+
+// let givenName = [
+//     {
+//         id: 1,
+//         age: 20,
+//         name: "Nuriddin",
+//         gmail: "nuriddin@gmail.com"
+//     },
+//     {
+//         id: 2,
+//         age: 15,
+//         name: "Komiljon",
+//         gmail: "komiljon@gmail.com"
+//     },
+//     {
+//         id: 3,
+//         age: 25,
+//         name: "Izzatbek",
+//         gmail: "Izzatbek@gmail.com"
+//     }
+// ];
+
+// for(let [key, value] of Object.entries(arr)){
+//     console.log(`${key}:${value}`);
+// }
