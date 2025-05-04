a8cc76cd3828e7a111d6b7d2651e1f6fc9941f68
Dilnozass <rakhma1ullaevaa@gmail.com>
2025-05-05 00:59:16 +0500

    add smth

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-//  ------ #TASK1 ------
-
-// let num1 = prompt("Birinchi sonni kiriting...")-0
-// let num2 = prompt("Ikkinchi sonni kiriting...")-0
-// let sum = num1 + num2
-
-// if (sum >= 0 && sum <= 9){
-//     console.log(String(sum))
-// }
-// else if (sum >= 10 && sum <= 99){
-//     Number(sum)
-//     console.log(Number(sum))
-// }
-// else{
-//     sum = null
-//     console.log(null)
-// }
-
-
-
-//  ------ #TASK2 ------
-
-// 1soat = 60minut
-// 1minut = 60sekund
-
-// let secund = prompt("Sekund kiriting...") // 7200 sekund = 120minut = 2soat
-// let hour = secund % min 
-// let min = 
-
-
-
 // ------ #TASK1 ------
 
 // let person = {
@@ -45,38 +14,122 @@
 //     console.log(`${key}:${value}`);
 // }
 
+// cars.sort(function (a, b){
+//     return a.price - b.price;
+// })
+// console.log(cars)
 
 
 
-// let arr = [15, 25, 46, 58, 91, 76, 27]
+//  ------ #TASK2 ------
 
-// for(i)
-
-
-
-
-
-// let givenName = [
-//     {
-//         id: 1,
-//         age: 20,
-//         name: "Nuriddin",
-//         gmail: "nuriddin@gmail.com"
-//     },
-//     {
-//         id: 2,
-//         age: 15,
-//         name: "Komiljon",
-//         gmail: "komiljon@gmail.com"
-//     },
-//     {
-//         id: 3,
-//         age: 25,
-//         name: "Izzatbek",
-//         gmail: "Izzatbek@gmail.com"
+// const arr1 = [5, 6, true, null, 8]
+// const arr2 = [0, 2, 5, false, 6]
+// const sumArr = arr1.concat(arr2)
+ 
+// let sum = 0
+// for(let i = 0; i < sumArr.length; i++){
+//     if (typeof sumArr[i] === 'number') {
+//         sum += sumArr[i]
 //     }
-// ];
-
-// for(let [key, value] of Object.entries(arr)){
-//     console.log(`${key}:${value}`);
 // }
+// console.log(sumArr)
+// console.log("Array sonlari yig'indisi:" , sum)
+ 
+
+
+//  ------ #TASK3 ------
+
+// let number = [87, 100, 54, 88, 57, 65, 43]
+// let smallNum = Math.min(... number)
+// let index = numbers.indexOf(smallNum)
+// console.log("Mavjud sonlar:", numbers)
+// console.log("Eng kichik son:", smallNum )
+// console.log("Eng kichik son indexi:", index)
+
+
+
+//  ------ #TASK4 ------
+
+// let num = [4, 23, 34, 12, 25];
+// let sum = 0;
+ 
+// for (let i = 0; i < num.length; i++) {
+//   sum += num[i];
+// }
+ 
+// function oddEven(num, sum) {
+//   if (num[0] % 2 === 0) {
+//     num.unshift(sum);  
+//   } 
+//   else {
+//     num.push(sum);    
+//   }
+// }
+ 
+// oddEven(num, sum);
+// console.log(num);
+
+
+
+// ----- #TASK5 ------
+
+// let arr1 = [5, 4, 3, 6, 3, 7, 1]
+// let arr2 =  [22, 1, 42, 34, 55, 67, 11, 32]
+// let bothArr = arr1.concat(arr2)
+
+// bothArr.sort(function(a, b){`
+//     return a - b
+// })
+
+
+
+//  ------ #TASK6 ------
+
+// let cars = [
+// {
+//     name:"Lacetti",
+//     price: 9000
+// },
+// {
+//     name:"Damas",
+//     price: 7000
+// },
+// {
+//     name:"Cobalt",
+//     price: 12500
+// },
+// {
+//     name:"Gentra",
+//     price: 14000
+// },
+// {
+//     name:"Onix",
+//     price: 15000
+// }
+// ]
+
+
+
+//  ------ #TASK7 ------
+
+
+// let arr = ["Hello", "What's up", "Najot Ta'lim", "Bye"]
+// for( let i = 0; i < arr.length; i++ ){
+//     if (i === 1 || i === 2) {
+//         console.log(arr[i]);}
+// }
+
+
+
+//  ------ #TASK8 ------
+
+// let word = prompt("Ixtiyoriy so'z/son kiriting...");
+ 
+// function teskari(word) {
+//     return word.split('').reverse().join('');
+// }
+ 
+// let result = teskari(word);
+// console.log(result);
+// alert(result);
